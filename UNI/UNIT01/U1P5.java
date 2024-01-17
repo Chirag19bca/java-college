1fae4615a192a046a065ac13f8395d1d3d51ea6c
Chirag19bca <119275895+Chirag19bca@users.noreply.github.com>
2024-01-17 14:36:14 +0530

    Create U1P5.java

---
+++ UNI/UNIT01/U1P5.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+class U1P5{
+	public static void main(String args[])
+	{
+		int x,y;
+		double hyp;
+		String in=JOptionPane.showInputDialog(null,"Enter value of x: ");
+		x=Integer.parseInt(in);
+		String in2=JOptionPane.showInputDialog(null,"Enter value of y: ");
+		y=Integer.parseInt(in2);
+		hyp=Math.sqrt(((x*x)+(y*y)));
+        System.out.println("Hypotenuse: "+hyp);
+    }
+}
